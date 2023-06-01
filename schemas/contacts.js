593bb3056df38fa26c3fe5de6f984131398b2e66
Bilogolovyi Sergii <sergey.hotchili@gmail.com>
2023-06-01 23:57:48 +0800

    fixed validation file and ms for important fields

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -5,15 +5,15 @@ const addSchema = Joi.object({
   name: Joi.string().required().messages({
     "string.base": `"name" should be a type of 'text'`,
     "string.empty": `"name" cannot be an empty field`,
-    "any.required": `"name" is a required field`,
+    "any.required": `missing required field "name"`,
   }),
   email: Joi.string().required().messages({
     "string.empty": `"email" cannot be an empty field`,
-    "any.required": `"email" is a required field`,
+    "any.required": `missing required field "email"`,
   }),
   phone: Joi.string().required().messages({
     "string.empty": `"phone" cannot be an empty field`,
-    "any.required": `"phone" is a required field`,
+    "any.required": `missing required field "phone"`,
   }),
 });
 
